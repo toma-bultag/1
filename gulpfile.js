5c3175846436e1ac4f57e257267a043c9bcaeaf2
Toma <toma.bultag@gmail.com>
2019-11-21 12:10:00 +0200

    Mid version

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,13 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('default', function() {
-	console.log("Horray - you created a Gulp task.");
-});
 
-gulp.task('html', function() {
-	console.log("Imagine something useful being done to your HTML here.");
-});
 
-gulp.task('styles', function() {
-	console.log("CSSSSS");
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));	
-});
 
-gulp.task('watch', function() {
 
-  watch('./app/index.html', function() {
-    gulp.start('html');
-  });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
-  });
-});
+
+
+
+
